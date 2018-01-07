df671902456503cdaec90a81d7c3115fec5f88b2
Siddharth <sid3637@gmail.com>
2018-01-07 18:02:53 +0000

    docs: added docstrings to helper functions

--- VehicleDetectionAndTracking/helper_functions.py
+++ VehicleDetectionAndTracking/helper_functions.py
@@ -8,6 +8,15 @@ from skimage.feature import hog
 import matplotlib.image as mpimg
 
 def draw_boxes(img, bboxes, color=(0, 255, 0), thick=6):
+    '''
+    Draw bounding boxes in an image
+    :param img(ndarray): Image
+    :param bboxes([2-element tuple]): List of bounding boxes, where each bounding box has the
+                form ((x1, y1), (x2, y2))
+    :param color(tuple): Color value for the bounding box
+    :param thickness(int): Thickness of the box
+    :return (ndarray): Updated image
+    '''
     # Make a copy of the image
     imcopy = np.copy(img)
 
@@ -16,11 +25,16 @@ def draw_boxes(img, bboxes, color=(0, 255, 0), thick=6):
         # Draw a rectangle given bbox coordinates
         cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
 
-    # Return the image copy with boxes drawn
     return imcopy
 
 
 def convert_color(img, cspace='HSV'):
+    '''
+    Converts the color space of an image
+    :param img(ndarray): Image
+    :param cspace(string): Color space type
+    :return (ndarray): Image with the new color space representation
+    '''
     if cspace == 'HSV':
         return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     elif cspace == 'HLS':
@@ -36,10 +50,24 @@ def convert_color(img, cspace='HSV'):
 
 
 def bin_spatial(img, size=(32, 32)):
+    '''
+    Performs spatial binning to reduce the image size and consequently the number
+    image pixels
+    :param img(ndarray): Image
+    :param size((int, int)): New image size
+    :return (ndarray): Updated image
+    '''
     return cv2.resize(img, size).ravel()
 
 
 def color_hist(img, nbins=32, bins_range=(0, 256)):
+    '''
+    Computes the histogram of color values
+    :param img(ndarray): Image
+    :param nbins(int): Number of histogram bins
+    :param bins_range(int): Lower and upper range of the bins
+    :return (ndarray): Histogram of color values
+    '''
     # Compute the histogram of the color channels separately
     channel1_hist = np.histogram(img[:, :, 0], bins=nbins, range=bins_range)
     channel2_hist = np.histogram(img[:, :, 1], bins=nbins, range=bins_range)
@@ -49,12 +77,23 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
     hist_features = np.concatenate(
         (channel1_hist[0], channel2_hist[0], channel3_hist[0]))
 
-    # Return the individual histograms, bin_centers and feature vector
     return hist_features
 
 
 def get_hog_features(img, orient=9, pix_per_cell=8, cell_per_block=2,
                      transform_sqrt=False, vis=False, feature_vec=True):
+    '''
+    Wrapper function to compute the HOG (Histogram of Oriented Gradients) feature descriptor
+    :param img(ndarray): Image
+    :param orient(int): Number of orientation bins
+    :param pix_per_cell(int): Number of pixels per cell
+    :param cell_per_block(int): Number of cells per block
+    :param transform_sqrt(boolean): Boolean flag to apply power law compression to normalize
+                    the image before processing
+    :param vis(boolean): Boolean flag to turn HOG visualisation on/off
+    :param feature_vec(boolean): Boolean flag on whether to return the unrolled feature vector or not
+    :return (ndarray): HOG Feature vector
+    '''
     return hog(img, orientations=orient,
                pixels_per_cell=(pix_per_cell, pix_per_cell),
                cells_per_block=(cell_per_block, cell_per_block),
@@ -67,6 +106,21 @@ def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
                      hist_bins=32, orient=9,
                      pix_per_cell=8, cell_per_block=2, hog_channel=0,
                      spatial_feat=True, hist_feat=True, hog_feat=True):
+    '''
+    Function to extract features from a single image/video frame
+    :param img(ndarray): Image
+    :param cspace(string): Color space in which to extract the features
+    :param spatial_size((int, int)): Spatial bin size; param for extracting raw color values
+    :param hist_bins(int): Number histogram bins; param for extracting Histogram of color values
+    :param orient(int): Number of orientation bins; param for extracting HOG features
+    :param pix_per_cell(int): Number of pixels per cell; param for extracting HOG features
+    :param cell_per_block(int): Number image channels to use for extracting the HOG features
+    :param hog_channel(int): Number of cells per block; param for extracting HOG features
+    :param spatial_feat(boolean): Boolean flag to extract the raw color values
+    :param hist_feat(boolean): Boolean flag to extract the histogram of color values
+    :param hog_feat(boolean): Boolean flag to extract the HOG features
+    :return (ndarray): Resulting feature vector
+    '''
 
     # 1) Define an empty list to receive features
     features = []
